87a9e61903d1272a28f7d8f69093433c08affec0
cruzetapiko <simonetvia@outlook.com>
2023-03-28 21:10:27 -0600

    animated text completed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import Head from "next/head";
-import Layout from "@/components/Layout";
-import Image from "next/image";
-import profilePicture from "@/public/images/profile/developer-pic-1.png";
+import Head from 'next/head';
+import Layout from '@/components/Layout';
+import Image from 'next/image';
+import profilePicture from '@/public/images/profile/developer-pic-1.png';
+import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
+import { LinkArrow } from '@/components/Icons';
 
 export default function Home() {
   return (
@@ -20,14 +23,36 @@ export default function Home() {
                 className="w-full h-auto"
               />
             </div>
-            <div className="w-1/2">
-              <h1>- Turning Vision Into Reality With Code And Design.</h1>
-              <p>
+            <div className="w-1/2 flex flex-col items-center self-center ">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-6xl text-left"
+              />
+              <p className="my-4 text-base font-medium ">
                 - As askilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2 ">
+                <Link
+                  href={'/dummy.pdf'}
+                  target={'_blank'}
+                  className="flex items-center bg-dark text-light text-lg font-semibold p-2.5 px-6 rounded-lg
+                  hover:bg-light hover:text-dark transition duration-300 ease-in-out
+                  border-2 border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Resume <LinkArrow className={'w-6 ml-1'} />
+                </Link>
+                <Link
+                  href={'mailto: simonetvia@outlook.com'}
+                  target={'_blank'}
+                  className="ml-4 text-lg text-dark underline capitalize font-medium"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
